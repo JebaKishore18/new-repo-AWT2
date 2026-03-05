803e67b10bef114efaa926fadcd537ea6c2b9d9c
JebaKishore18 <nadarkishore2@gmail.com>
2026-03-05 14:46:47 +0530

    Update jen.java

--- AWT_2/jen.java
+++ AWT_2/jen.java
@@ -1,6 +1,6 @@
 public class jen{
     public static void main(String[] args) {
-        System.out.println("I love you so much");
+        System.out.println("I love you so very much");
         System.out.println("Kishore");
     }
 }
